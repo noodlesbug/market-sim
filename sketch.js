3155cb059e66ab684fb1762dd328b707030c9bfe
noodlesbug <91686708+noodlesbug@users.noreply.github.com>
2025-03-26 13:58:36 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(600, 400);
+    background(240);
+    textAlign(CENTER, CENTER);
+    textSize(18);
+    text("Coming soon: interactive market simulation!", width / 2, height / 2);
+  }
+  
